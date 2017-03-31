ade24faf3d6800d627f70edf73443d36eda7e716
popovsergiy <popow.sergiy@gmail.com>
2017-03-31 22:35:35 +0300

    Add skin files. Add map

---
+++ skin/frontend/base/default/agere/service/js/service.js
@@ -0,0 +1,38 @@
+var AgereService = {
+	elm: null,
+
+	fancyOptions: {
+		'ajax': {cache: true, dataType: 'html'},
+		'minWidth': 300,
+		'afterShow': function (prev, curr) {
+			AgereService.elm.trigger('fb.afterShow', [prev, curr]);
+		},
+		'beforeLoad': function() {
+			this.ajax.data = {
+				'referUrl': window.location.href
+			};
+			AgereService.elm.trigger('fb.beforeLoad');
+		},
+	},
+
+	activate: function() {
+		console.log(1111111111);
+		var self = AgereService;
+		self.elm = jQuery(this);
+		var options = jQuery.extend(true, self.fancyOptions, self.elm.data('options').fancybox);
+		// @see http://stackoverflow.com/a/13948085/1335142
+		jQuery.fancybox.open([
+			options
+		], {
+			padding : 0
+		});
+	},
+
+	capitalizeFirstLetter: function (string) {
+		return string[0].toUpperCase() + string.slice(1);
+	}
+};
+
+//jQuery(document).ready(function($) {
+//	AgereService.init();
+//});
